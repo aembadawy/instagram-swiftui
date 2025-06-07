102a67228dc3452a7403ec2de8aeca51060f326b
aembadawy <aem.badawy@gmail.com>
2025-06-07 19:05:29 +0300

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Aya on 07/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstagramTests/InstagramTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTests.swift
+//  InstagramTests
+//
+//  Created by Aya on 07/06/2025.
+//
+
+import Testing
+@testable import Instagram
+
+struct InstagramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
