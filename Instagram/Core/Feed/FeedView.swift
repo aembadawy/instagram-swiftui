752c038198f3e888cfe2e640cd92bd6791456f73
aembadawy <aem.badawy@gmail.com>
2025-06-08 16:30:36 +0300

    Add feed item

---
+++ Instagram/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  Instagram
+//
+//  Created by Aya on 08/06/2025.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
