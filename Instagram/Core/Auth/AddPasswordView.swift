3d2ed34394b74a2cbe623256a840224d2b08c3fc
aembadawy <aem.badawy@gmail.com>
2025-06-10 09:55:37 +0300

    Add password view

--- Instagram/Core/Auth/AddPasswordView.swift
+++ Instagram/Core/Auth/AddPasswordView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct AddPasswordView: View {
+    @State var password = ""
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Text("Add your password")
+                .font(.title3)
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            Text("Password must be 9-16 characters long and must contain capital letter and a speacial character.")
+                .multilineTextAlignment(.center)
+                .font(.footnote)
+                .foregroundStyle(.gray)
+                .padding(.horizontal, 24)
+            
+            SecureField("Enter your password", text: $password)
+                .modifier(IGTextFieldModifier())
+            
+            NavigationLink {
+                FlowCompletedView()
+                    .navigationBarBackButtonHidden()
+            } label: {
+                Text("Next")
+                    .modifier(CustomButtonModifier())
+            }
+            
+            Spacer()
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
     }
 }
 
