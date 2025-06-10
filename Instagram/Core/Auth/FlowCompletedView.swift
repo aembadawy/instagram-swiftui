ef9f3439835a057a894ba746e50d9fb9d599b7dd
aembadawy <aem.badawy@gmail.com>
2025-06-10 09:56:21 +0300

    Add complete flow view

--- Instagram/Core/Auth/FlowCompletedView.swift
+++ Instagram/Core/Auth/FlowCompletedView.swift
@@ -1,5 +1,5 @@
 //
-//  FlowCompleted.swift
+//  FlowCompletedView.swift
 //  Instagram
 //
 //  Created by Aya on 09/06/2025.
@@ -7,12 +7,43 @@
 
 import SwiftUI
 
-struct FlowCompleted: View {
+struct FlowCompletedView: View {
+    @Environment(\.dismiss) var dismiss
+    var username = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Text("Welcome to Instagram \(username)")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            
+            Text("Click below to complete registration and start using Instagram.")
+                .font(.footnote)
+                .fontWeight(.light)
+                .foregroundStyle(.gray)
+                .multilineTextAlignment(.center)
+            
+            Button {
+                
+            } label: {
+                Text("Complete Sign Up")
+                    .modifier(CustomButtonModifier())
+                    .padding(.top)
+            }
+
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
     }
 }
 
 #Preview {
-    FlowCompleted()
+    FlowCompletedView()
 }
