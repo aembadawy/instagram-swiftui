fa7f5a6a537edb531b034dfeba71c83ff3f5154d
aembadawy <aem.badawy@gmail.com>
2025-06-10 09:55:53 +0300

    Add username view

--- Instagram/Core/Auth/AddUserNameView.swift
+++ Instagram/Core/Auth/AddUserNameView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct AddUserNameView: View {
+    
+    @State var username: String = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16){
+            Text("Add your username")
+                .font(.title3)
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            Text("Pick a username to your account. You can always change it later.")
+                .multilineTextAlignment(.center)
+                .font(.footnote)
+                .foregroundStyle(.gray)
+                .padding(.horizontal, 24)
+            
+            TextField("Username", text: $username)
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+                .modifier(IGTextFieldModifier())
+            
+            NavigationLink {
+                AddPasswordView()
+                    .navigationBarBackButtonHidden()
+            } label: {
+                Text("Next")
+                    .modifier(CustomButtonModifier())
+            }
+
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
     }
 }
 
