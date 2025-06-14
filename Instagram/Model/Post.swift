28903e66e9e4a4c600da67910a6f44abd4ffac80
aembadawy <aem.badawy@gmail.com>
2025-06-14 14:07:36 +0300

    Add post model

--- Instagram/Model/Post.swift
+++ Instagram/Model/Post.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+struct Post: Identifiable, Codable, Hashable {
+    let id: String
+    let ownerId: String
+    let caption: String
+    var likes: Int
+    let timestamp: Date
+    let imageURL: String
+    var user: User?
+    
+}
+
+extension Post {
+    static var MOCK_POSTS: [Post] = [
+     
+            Post(
+                id: UUID().uuidString,
+                ownerId: User.MOCK_USERS[0].id,
+                caption: "Enjoying the sunset 🌅",
+                likes: 120,
+                timestamp: Date().addingTimeInterval(-3600), // 1 hour ago
+                imageURL: "images-5",
+                user: User.MOCK_USERS[0]
+            ),
+            Post(
+                id: UUID().uuidString,
+                ownerId: User.MOCK_USERS[1].id,
+                caption: "Morning coffee vibes ☕️",
+                likes: 89,
+                timestamp: Date().addingTimeInterval(-7200), // 2 hours ago
+                imageURL: "images-3",
+                user: User.MOCK_USERS[1]
+            ),
+            Post(
+                id: UUID().uuidString,
+                ownerId: User.MOCK_USERS[2].id,
+                caption: "Hiking up the mountains! 🏞",
+                likes: 150,
+                timestamp: Date().addingTimeInterval(-10800), // 3 hours ago
+                imageURL: "images-4",
+                user: User.MOCK_USERS[2]
+            )
+        ]
+    
+}
